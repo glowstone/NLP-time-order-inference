f64e7192cec154f95b6b673acbab12edfd019f6a
Thomas Garver <tgarv@mit.edu>
2012-12-11 22:10:08 -0500

    Updated the greater than/less than operators for Dates. I think they work now.

--- date_models.py
+++ date_models.py
@@ -14,39 +14,60 @@ class Date():
 		self.minute = minute
 
 	def __lt__(self, other):
+		"""
+		Tries to determine if self < other. While doing so, it tries to resolve ambiguities and admit when it can't
+		determine whether one is less than the other.
+
+		other is a Date object
+
+		returns: boolean or "Can't tell" if it can't tell
+		"""
 		rep = [self.year, self.month, self.day, self.hour, self.minute]
 		other_rep = [other.year, other.month, other.day, other.hour, other.minute]
 		rep2 = []
 		other_rep2 = []
 
-		# Basically we need to get rid of cases where we're trying to compare something like "2003" with "January 5".
-		# In that case, we really can't do anything so we'll return "Can't tell". But, if it's like comparing "2003"
-		# with "January 5, 2002" then we should do the comparison on the years. The following basically gets rid of
-		# the fields that they don't both have, and compares what's left over if possible
-		for i in xrange(len(rep)):
-			if rep[i] is not None and other_rep[i] is not None:
-				rep2.append(rep[i])
-				other_rep2.append(other_rep[i])
-			elif rep[i] is None and other_rep[i] is None:
-				continue
-			else:
-				break
+		for i in xrange(len(other_rep)):
+			item1 = rep[i]
+			item2 = other_rep[i]
 
-		print rep2, other_rep2
-		if not (rep2 and other_rep2):
-			return "Can't tell"
-		for i in xrange(len(rep2)):
-			if rep2[i] is not None:
-				if other_rep2[i] is None:
+			# Basically going to iterate through rep and other_rep, and compare the parts that are present in both of
+			# them. If anything is ambiguous, return "Can't tell"
+			if item1 is None:
+				if item2 is not None:
+					if rep2 == other_rep2:
+						# It's been equal up to this point, but now other is more specific than self so they
+						# kind of happened at the same time
+						return "Can't tell"
+				# Year was not defined for either, so we can't really do much
+				if rep2 == [] and other_rep2 == []:
 					return "Can't tell"
-			else:
-				if other_rep2[i] is not None:
-					return "Can't tell"
+				return rep2 < other_rep2
 
-		return rep < other_rep
+			if item2 is None:
+				if item1 is not None:
+					if rep2 == other_rep2:
+						# It's been equal up to this point, but now self is more specific than other so they
+						# kind of happened at the same time
+						return "Can't tell"
+				# Year was not defined for either, so we can't really do much
+				if rep2 == [] and other_rep2 == []:
+					return "Can't tell"
+				return rep2 < other_rep2
+
+			else:	# Otherwise they both have this item, so we can append it and continue
+				rep2.append(item1)
+				other_rep2.append(item2)
+				
+		# This is the case where both had all 5 defined, then just compare the lists.
+		return rep2 < other_rep2
+
 
 	def __gt__(self, other):
 		return other.__lt__(self)
+
+	def __eq__(self, other):
+		return [self.year, self.month, self.day, self.hour, self.minute] == [other.year, other.month, other.day, other.hour, other.minute]
 
 	def __repr__(self):
 		return "Date %s %s %s %s %s" % (self.year, self.month, self.day, self.hour, self.minute)
